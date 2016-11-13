0cb1c6e9d9d9dcdd631ef5f238d58369ec64cc5f
Pirave <pirave@gmail.com>
2016-11-12 20:23:06 -0500

    TTC Update

--- slack/views.py
+++ slack/views.py
@@ -1,13 +1,26 @@
 from django.http import JsonResponse
 from django.shortcuts import render
 
-from .forms import IsitupForm
+from .forms import IsitupForm, TTCForm
 
 from isitup.main import check
 
 def check_isitup(request):
 	if request.method == 'POST':
 		form = IsitupForm(request.POST)
+		if form.is_valid():
+			text = form.cleaned_data['text']
+
+			response = check(text)
+
+			return JsonResponse({'text': response})
+
+	return JsonResponse({'slack-commands': ['isitup']})
+
+
+def check_ttc(request):
+	if request.method == 'POST':
+		form = TTCForm(request.POST)
 		if form.is_valid():
 			text = form.cleaned_data['text']
 
