4632de23a2cbbd9b7b7cb887810f86dc02a7b9bb
Pirave <pirave@gmail.com>
2016-11-12 20:45:18 -0500

    adding nextbus api

--- slack/views.py
+++ slack/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from .forms import IsitupForm, TTCForm
 
 from isitup.main import check
+import requests
 
 def check_isitup(request):
 	if request.method == 'POST':
@@ -24,8 +25,11 @@ def check_ttc(request):
 		if form.is_valid():
 			text = form.cleaned_data['text']
 
-			response = check(text)
+			# r = requests.get('http://webservices.nextbus.com/service/publicXMLFeed?command=routeConfig&a=ttc&r=%s' % text)
+			url = 'http://webservices.nextbus.com/service/publicXMLFeed?command=routeConfig&a=ttc&r=' + text
+			headers = { 'Accept-Encoding': 'gzip, deflate', }
+			r = requests.get(url, headers = headers)
 
-			return JsonResponse({'text': response})
+			return JsonResponse({'text': t.text})
 
-	return JsonResponse({'slack-commands': ['isitup']})
+	return JsonResponse({'slack-commands': ['ttc']})
